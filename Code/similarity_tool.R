eb0e34ccabdd9b2461baa315855d3163ee838ef9
alextabascio <alex.tabascio@mail.utoronto.ca>
2024-06-19 13:40:53 -0400

    added base housing stock tool

--- Code/similarity_tool.R
+++ Code/similarity_tool.R
@@ -29,7 +29,7 @@ options(scipen = 999)
 
 # ------------------------------------------------------------------------------------------------
 
-#### CREATING THE SIMILARITY SCORE FUNCTION ####
+#### SIMILARITY SCORE FUNCTION ####
 
 # this function will take in a list of road segments and output a spatial data frame
 # of similar streets
@@ -114,12 +114,67 @@ similar_streets = function(road_network, list_of_streets){
   final_set = test_roads %>%
     filter(sim_index >= 0.75)
   
+  
+  final_set = road_network %>%
+    select(id) %>%
+    inner_join(final_set, by = "id")
+  
   # return the final set
   return(final_set)
 }
 
 # -----------------------------------------------------------------------------------------------------
 
+#### BASE HOUSING COUNT FUNCTION ####
+
+get_casestudy_units = function(road_network, location_data, list_of_streets){
+  
+  # filter the road network based on the list of streets
+  initial_roads = road_network %>%
+    filter(!is.na(per_business_density) & id %in% list_of_streets)
+  
+  # turn the location data as a sptail data frame
+  location_data = location_data %>%
+    filter(!is.na(reppoint_latitude))
+  location_data_st = st_as_sf(location_data, coords = c("reppoint_longitude", "reppoint_latitude"), crs = 4326) %>%
+    st_transform(crs = 3347)
+  
+  # concatenate the address field
+  location_data_st = location_data_st %>%
+    mutate(
+      civic_no = as.character(civic_no),
+      civic_no = ifelse(civic_no == "", NA, civic_no),
+      official_street_name = ifelse(official_street_name == "", NA, official_street_name),
+      official_street_type = ifelse(official_street_type == "", NA, official_street_type),
+      official_street_dir = ifelse(official_street_dir == "", NA, official_street_dir),
+      # combine the address
+      Address = str_c(
+        if_else(!is.na(civic_no), str_c(civic_no), ""),
+        if_else(!is.na(official_street_name), str_c(" ", official_street_name), ""),
+        if_else(!is.na(official_street_type), str_c(" ", official_street_type), ""),
+        if_else(!is.na(official_street_dir), str_c(" ", official_street_dir), ""),
+        sep = ""
+      )
+    ) %>%
+    select(loc_guid, addr_guid, Address, bu_use)
+  
+  
+  # create an intersection between the buffered roads and the location data
+  location_data_clipped = st_intersection(st_buffer(initial_roads %>% select(), 250), location_data_st)
+  
+  # with the initial data clipped preform a nearest neighbour join for the road segment id
+  case_study_locations = st_join(location_data_clipped, initial_roads, join = st_nearest_feature)
+  
+  # get the summary of units for each address
+  # used for internal testing
+  address_summary = case_study_locations %>%
+    group_by(id, Address) %>%
+    summarise(units = n())
+  
+  # return the address points
+  return(address_summary)
+  
+}
 
 
 
@@ -127,6 +182,36 @@ similar_streets = function(road_network, list_of_streets){
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------------
+
+
+# load in msn_base data
+setwd("C:/Users/atabascio/CUI/Projects - External - Documents/819. Research & Knowledge Initiative – INFC/3 - Background Data & Research/GIS Map prototype/RKI_MainStreetMatters")
+msn_base = st_read("./Interim/MainStreetRoadNetwork/CANADA/mainstreet_allmetrics/geojsons/msn_base_all.geojson") %>%
+  select((1:10), per_business_density, per_civic_density, per_employment_density, per_business_independence_index,
+         per_population_change, per_population_density, per_visible_minorities, per_indigenous, per_immigrants_non_permanent_residents,
+         per_average_employment_income) %>%
+  filter(!is.na(per_population_change)) %>%
+  st_transform(crs = 3347)
+
+
+# load in the address and location data for Manitoba
+setwd("~/cmhc-scaling")
+location_data = read.csv("./Data/address_data/joined_addresses_46.csv") %>%
+  select((2:9), bu_use, reppoint_latitude, reppoint_longitude, -civic_no_suffix) %>%
+  filter(bu_use == 1 | bu_use == 2)
 
 
 # create a subset based on street ids
@@ -135,16 +220,6 @@ montreal_rd = c("118659", "188525")
 elice_ave = c("95731", "95779")
 
 
-final_montreal_rd = similar_streets(msn_base_final, montreal_rd)
-
-final_elice_ave = similar_streets(msn_base_final, elice_ave)
-
-
-final_elice_ave = msn_base %>%
-  select(id) %>%
-  inner_join(final_elice_ave, by = "id")
-
-st_write(final_elice_ave, "./Ouput/final_elice_ave.geojson", driver = "GeoJSON")
 
 
 
